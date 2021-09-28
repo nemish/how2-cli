c87b2669abff7d43a20cd2316a406eb22db07c39
Yaroslav Melnichuk <nemish.i.nelos@gmail.com>
2021-09-28 20:11:20 +0300

    fix: added welcom

--- index.ts
+++ index.ts
@@ -1,29 +1,25 @@
 #! /usr/bin/env node
 import { Command } from "commander";
+import chalk from "chalk";
+
 import { readFile } from "fs";
 const program = new Command();
 program.version("0.0.1");
 
 program
   .option("-d, --debug", "output extra debugging")
+  .option("-w, --welcome", "Welcome message")
   .option("-f, --file <type>", "filename");
 
 program.parse(process.argv);
 
 const options = program.opts();
 if (options.debug) console.log(options);
-console.log("Log:");
 if (options.file) console.log(`- ${options.file}`);
 
-// const readFile = (filepath) => {
-//   fs.readFile(filepath, 'utf8' , (err, data) => {
-//     if (err) {
-//       console.error(err)
-//       return
-//     }
-//     console.log(data)
-//   })
-// }
+if (options.welcome) {
+  console.log(chalk.blue("Hello world!"));
+}
 
 readFile(
   options.file || "README.md",
